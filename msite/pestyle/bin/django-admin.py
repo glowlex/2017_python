64e9548fb7ae16f60a49583bef8d440b83f71b53
glowlex <navsl@yandex.ru>
2017-04-03 01:32:35 +0300

    make setup.h ready for easy deploy

---
+++ msite/pestyle/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/nv/2017_python/msite/pestyle/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ setup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# в конфиге нжинх поменять путь если надо
+
+#sudo -s
+add-apt-repository ppa:jonathonf/python-3.6
+apt-get update
+apt-get install python3.6
+pip3 install --upgrade pip
+pip3 install --upgrade virtualenv
+virtualenv -p python3.6 pestyle
+source pestyle/bin/activate
+pip3 install django==1.10.6
+easy_install Celery
+pip3 install gunicorn
+apt-get install nginx
+
+
+rm /etc/nginx/sites-enabled/pestyle_nginx.conf
+cp ./pestyle_nginx.conf /etc/nginx/sites-available/
+ln -s /etc/nginx/sites-available/pestyle_nginx.conf /etc/nginx/sites-enabled/
+service nginx restart
+
+python ./msite/manage.py collectstatic
+
+cd ./msite
+gunicorn msite.wsgi:application
+
+#python manage.py createsuperuser
+#python manage.py migrate
+#python manage.py runserver
