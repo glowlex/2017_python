af9b79ccbb49d33757e4803a558786ec661c664e
glowlex <navsl@yandex.ru>
2017-05-03 01:51:33 +0300

    nginx, docker fix

--- start_doc.sh
+++ start_doc.sh
@@ -5,7 +5,7 @@
 sudo apt-get update
 sudo apt-get install wget
 wget -qO- https://get.docker.com/ | sh
-docker run -v /opt/project/2017_python/msite/pestyle/static:/var/www/static -v /opt/project/2017_python/msite/:/var/www/sql/  -t -p 8080:8080 glowlex/pestyle
+docker run -v /opt/project/2017_python/msite/pestyle/static:/var/www/static -v /opt/project/2017_python/msite/:/var/www/sql/  -t -p 80:80 glowlex/pestyle
 
 #sudo docker exec -i -t  nifty_mestorf bash
 #docker run -it -d 71f7beea52aa
